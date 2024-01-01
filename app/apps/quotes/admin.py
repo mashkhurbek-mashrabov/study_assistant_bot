5acc44e47ac6492f724b7f975b2f64326af75db3
Mashhurbek Mashrabov <mashkhurbek.mashrabov@gmail.com>
2024-01-01 17:00:30 +0500

    added Author, Quote models

--- app/apps/quotes/admin.py
+++ app/apps/quotes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from quotes.models import Author, Quote
+
+admin.site.register(Author)
+
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('text', 'author', 'is_active', 'created_at')
+    list_filter = ('is_active', 'groups', 'author', 'created_at')
+    search_fields = ('text',)
--- app/apps/quotes/models.py
+++ app/apps/quotes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from common.models import BaseModel
+from university.models import Group
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Quote(BaseModel):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='quotes', blank=True, null=True)
+    text = models.TextField()
+    groups = models.ManyToManyField(Group, related_name='quotes')
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.text[0:100]
