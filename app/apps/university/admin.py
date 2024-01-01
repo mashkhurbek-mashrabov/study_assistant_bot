c374730b272a526f6e4cd8dc2c29041f32f03269
Mashhurbek Mashrabov <mashkhurbek.mashrabov@gmail.com>
2024-01-01 15:58:41 +0500

    added group, student models

--- app/apps/university/admin.py
+++ app/apps/university/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from university.models import Group, Student
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'telegram_id',)
+    search_fields = ('name', 'telegram_id',)
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'group', 'phone', 'telegram_id',)
+    list_filter = ('group', 'created_at',)
+    search_fields = ('name', 'telegram_id',)
--- app/apps/university/models.py
+++ app/apps/university/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from common.models import BaseModel
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    telegram_id = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(BaseModel):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='students')
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=13)
+    telegram_id = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
